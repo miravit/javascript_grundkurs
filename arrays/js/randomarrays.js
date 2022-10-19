283156fdc1302d783ab624e2735865d975d98439
MAC <mac@miras-MBP.lan>
2022-10-19 21:36:36 +0200

    förbättringar

--- arrays/js/randomarrays.js
+++ arrays/js/randomarrays.js
@@ -3,19 +3,23 @@ userButton.addEventListener("click" ,showName )
 
 let nummer =["kaffe, bröd, mjölk, tomat, choklad"];
 console.log(nummer)
+let found = false;
+let userInput="";
+
 
 function showName () {
-let userInput = document.getElementById("userinput").value;
+    
+    for (i=0; i<nummer.length; i++) {
+    let userInput = document.getElementById("userinput").value;
+    nummer.push(userInput);
+    console.log(nummer);
+    let textArea = document.getElementById("textarea");
+    textArea.innerHTML+= userInput+"+"
+    break;
+    }
 
-nummer.push(userInput);
-console.log(nummer);
 
-let textArea = document.getElementById("textarea");
-textArea.innerHTML+= userInput + " las nu till i listan i konsollen!"
 
-for (let i=0; i<nummer.lenght; i++){
-    console.log(nummer[i]+" ");
-}
 }
 
 //hhär ovan har jag pushat in min input i min array.
